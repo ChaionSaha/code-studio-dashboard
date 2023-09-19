a7e604eb6d9823c0479c79a5c2e9b79349c14e23
Chaion Saha <chaionsaha00@gmail.com>
2023-09-19 16:43:42 +0600

    initiated project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,29 @@ module.exports = {
 	content: ['./**/*.{html,js}'],
 	theme: {
 		extend: {},
+		screens: {
+			sm: '576px',
+			md: '768px',
+			lg: '992px',
+			xl: '1200px',
+			'2xl': '1400px',
+		},
 	},
-	plugins: [],
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#185ABC',
+					secondary: '#00965C',
+					accent: '#280559',
+					'base-100': '#ffffff',
+					'base-200': '#8B909A ',
+					'base-300': '#fafafa',
+					success: '#11AF22 ',
+					error: '#DB0D4B ',
+				},
+			},
+		],
+	},
 };
